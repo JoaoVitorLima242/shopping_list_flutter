fed0743c2ee64dd1acd296bfcb22a130408c3ddf
JoaoVitorLima242 <jlima0500@gmail.com>
2023-08-30 22:13:51 -0300

    adding buttons

--- lib/features/new_item/widgets/new_item_form.dart
+++ lib/features/new_item/widgets/new_item_form.dart
@@ -52,7 +52,21 @@ class NewItemForm extends StatelessWidget {
                 ),
               )
             ],
-          )
+          ),
+          const SizedBox(height: 12),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              TextButton(
+                onPressed: () {},
+                child: const Text('Reset'),
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                child: const Text('Add item'),
+              )
+            ],
+          ),
         ],
       ),
     );
